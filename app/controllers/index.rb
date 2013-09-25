4939c79cf00f9f06466f5cf1b1b05402ec2f7f5b
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-25 09:43:49 -0500

    established routing and database models

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,41 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
+
+
+get '/comments/:id' do
+  @post = Post.find(params[:id])
+  erb :comments
+end 
+
+get '/logout' do 
+  session[:id].destroy
+  end
+  erb :index
+end
+
+get '/create_post' do
+  erb :create_post
+end 
+
+post '/login' do 
+  @user = User.find_by_username(params[:username])
+  if @user 
+    if @user.password == params[:password]
+      session[:id] = @user.id
+      erb :index
+    else  
+      @notpass = 'Either your usernamd or password is wrong' 
+      redirect '/'
+    end 
+  else
+    @notpass = 'Either your usernamd or password is wrong'
+    erb :index
+  end 
+end
+
+
+# post '/create_post/:user_id' do
+#   @url = Post.create(params[:url], params[:user_id])
+#   erb :
+# end 
